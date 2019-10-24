a1646f504274ae06bf0bc5d17f4dff3652db296b
Baptiste Crouzet <contact@baptistecrouzet.fr>
2019-10-24 12:28:31 +0200

    Add a mobile header menu

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+// MOBILE MENU
+
+let burger = document.getElementById('burger'),
+    nav = document.getElementById('main-nav');
+
+burger.addEventListener('click', function () {
+    this.classList.toggle('is-open');
+    nav.classList.toggle('is-open');
+});
+
+/* Onload demo - dirty timeout */
+let clickEvent = new Event('click');
+
+window.addEventListener('load', function () {
+    setTimeout(function () {
+        burger.dispatchEvent(clickEvent);
+    }, 5500);
+});
